d4ea81da3028e58e12e21be4b8a3a91005a68805
SilentYanwu <1459134925@qq.com>
2025-10-05 15:30:14 +0800

    课程一第二周编程作业

---
+++ README.md
@@ -0,0 +1,6 @@
+## Andrewng DL Code
+
+本项目主要是学习**吴恩达老师深度学习**课后作业的项目实践
+因为本人对J*upter Notebook*十分无语，
+所以直接跟随**何宽大大**的CSDN帖子：https://blog.csdn.net/u013733326/article/details/79827273 进行学习
+这里直接用py脚本加上何老师的材料文件，直接完成项目。
---
+++ 通用修复路径脚本.py
@@ -0,0 +1,16 @@
+import os,sys
+# 添加路径修复代码
+def fix_paths():
+    """修复导入路径和文件路径"""
+    # 将当前文件所在目录添加到Python路径
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    if current_dir not in sys.path:
+        sys.path.insert(0, current_dir)
+    
+    # 切换到当前文件所在目录
+    os.chdir(current_dir)
+
+# 在导入lr_utils之前调用
+fix_paths()
+
+from lr_utils import load_dataset
